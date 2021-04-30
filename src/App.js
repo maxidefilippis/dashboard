8dc4dc9c8583b43b717c48ad420a6ce45548dd98
maxidefilippis <maxidefilippis@gmail.com>
2021-04-30 00:00:35 -0300

    [UPDATE] Styles folders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Home, About, Users, Login, Registration } from "./pages";
+import "../src/main.scss";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
         <p>DASHBOARD ADMIN</p>
       </header>
       <Router>
-        <div>
-          <nav>
+        <div className="header">
+          <nav className="header__nav">
             <ul>
               <li>
                 <Link to="/">Home</Link>
