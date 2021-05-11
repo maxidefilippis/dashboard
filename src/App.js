4b3f50055ffb1805cb9b72846cd32cb2db1cb44c
maxidefilippis <maxidefilippis@gmail.com>
2021-05-10 23:56:29 -0300

    [UPDATE] Header styles

--- src/App.js
+++ src/App.js
@@ -6,52 +6,52 @@ import "../src/main.scss";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>DASHBOARD ADMIN</p>
-      </header>
       <Router>
-        <div className="header">
-          <nav className="header__nav">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/users">Users</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/registration">Register</Link>
-              </li>
-            </ul>
-          </nav>
+        <header className="App-header">
+          <div className="header">
+            <p>DASHBOARD ADMIN</p>
+            <nav className="header__nav">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/registration">Register</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
 
-          <Switch>
-            <Route exact path="/about">
-              <About />
-            </Route>
-            <Route exact path="/users">
-              <Users />
-            </Route>
-            <Route exact path="/registration">
-              <Registration />
-            </Route>
-            <Route exact path="/login">
-              <Login />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="*">
-              <div>Not found</div>
-            </Route>
-          </Switch>
-        </div>
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/users">
+            <Users />
+          </Route>
+          <Route exact path="/registration">
+            <Registration />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <div>Not found</div>
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
